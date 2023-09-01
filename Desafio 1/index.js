5e3d0f158e9a275ab92fd9387b5a4b38b2237d7f
Ana Silva <112342313+analusilvaa@users.noreply.github.com>
2023-08-31 21:32:26 -0300

    Add files via upload

---
+++ Desafio 1/index.js
@@ -0,0 +1,37 @@
+var hero = "Ana Silva";
+var xp = 15000;
+var nivel;
+
+if(xp<1000){
+    nivel= "Ferro";
+} else{
+    if(xp>1000 && xp<=2000){
+        nivel= "Bronze";
+    } else{
+        if(xp>2000 && xp<=5000){
+            nivel="Prata";
+        } else{
+            if(xp>6000 && xp<=7000){
+                nivel="Ouro";
+            } else{
+                if(xp>7000 && xp<=8000){
+                    nivel="Platina";
+                } else{
+                    if(xp>8000 && xp<=9000){
+                        nivel="Ascendente";
+                    } else{
+                        if(xp>9000 && xp<=10000){
+                            nivel="Imortal";
+                        } else{
+                            if(xp>10000){
+                                nivel="Radiante";
+                            }
+                        }
+                    }
+                }
+            }
+        } 
+    }
+}
+
+console.log("O Herói de nome", {hero}, "está no nível", {nivel});
